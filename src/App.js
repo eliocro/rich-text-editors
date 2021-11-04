a0cc5eab6a7436afa2b7214c4ca8c23e8f91e434
Élio Cró <elio.cro@gmail.com>
2021-11-04 17:23:21 +0000

    Update readme and add link to github

--- src/App.js
+++ src/App.js
@@ -6,26 +6,40 @@ import Draft from './components/draft';
 import Preview from './components/preview';
 
 const editors = [
-  ['tiny',  'TinyMCE v5', Tiny],
-  ['draft', 'Draft.js',   Draft],
-  ['preview', 'Preview',   Preview],
+  ['tiny', 'TinyMCE v5', Tiny],
+  ['draft', 'Draft.js', Draft],
+  ['preview', 'Preview', Preview],
 ];
 
 export default function App() {
   return (
     <div className="wrapper">
-      <h1>Rich text editor comparison</h1>
       <HashRouter>
-        <Nav />
+        <header className="header">
+          <h1>Rich text editor comparison</h1>
+          <Nav />
+        </header>
         <Switch>
-          { editors.map(([path, _, Component], idx) => (
-            <Route key={idx} path={`/${path}`} component={ Component } />
-          )) }
+          {editors.map(([path, _, Component], idx) => (
+            <Route key={idx} path={`/${path}`} component={Component} />
+          ))}
           <Route>
-            <p>Pick an editor from the menu. Or check the Preview on the right.</p>
+            <p>
+              Pick an editor from the menu. Or check the Preview on the right.
+            </p>
           </Route>
         </Switch>
       </HashRouter>
+      <footer className="footer">
+        <span>Source code: </span>
+        <a
+          href="https://github.com/eliocro/rich-text-editors"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Github
+        </a>
+      </footer>
     </div>
   );
 }
@@ -34,12 +48,14 @@ function Nav() {
   return (
     <nav className="main-nav">
       <ul className="list">
-        { editors.map(([path, name], idx) => (
+        {editors.map(([path, name], idx) => (
           <li key={idx}>
-            <NavLink to={`/${path}`} activeClassName="active">{name }</NavLink>
+            <NavLink to={`/${path}`} activeClassName="active">
+              {name}
+            </NavLink>
           </li>
-        )) }
+        ))}
       </ul>
     </nav>
-  )
+  );
 }
